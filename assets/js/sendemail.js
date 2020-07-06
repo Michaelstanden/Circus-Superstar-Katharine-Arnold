00a7bcb3838f1c0bc8121cb19d2cecb55c425bd5
Michaelstanden <michaelstanden@hotmail.com>
2020-07-06 22:10:01 +0000

    added js for email.js and altered the form to match entry names

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,1 +1,14 @@
-console.log("hello");
+function sendMail(contactForm) {
+    emailjs.send("gmail", "katharine", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "artist_enquiry": contactForm.message,
+    })
+    .then(
+        function(response){
+            console.log("Success", response);
+        },
+        function(error){
+            console.log("failed", error);
+        });
+}
